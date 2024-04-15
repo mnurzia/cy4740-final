9bc88989164d8e50ee071e72dbfc5675b50938be
willjliu02 <william.liu1107@gmail.com>
2024-04-14 23:35:19 -0400

    commented passdb utils

--- passdb.py
+++ passdb.py
@@ -2,7 +2,9 @@ import json
 
 from util import *
 
-
+'''
+A helper to load in a json of the list of username and passwords
+'''
 def load_pdb(path: str) -> dict[str, tuple[bytes, bytes]]:
     out = {}
     with open(path, "r") as f:
@@ -11,7 +13,9 @@ def load_pdb(path: str) -> dict[str, tuple[bytes, bytes]]:
             out[id] = (u64(salt), u64(pwd))
     return out
 
-
+'''
+A helper to save the json of username and passwords into a json file
+'''
 def save_pdb(path: str, pdb: dict[str, tuple[bytes, bytes]]):
     out = {id: [b64(salt), b64(pwd)] for id, (salt, pwd) in pdb.items()}
     with open(path, "w") as f:
